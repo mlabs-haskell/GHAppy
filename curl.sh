e622773e60f051e8cabe838c4859e0abf5c681ea
Vlad P. Luchian <cstmlcodes@gmail.com>
2022-12-06 18:52:58 +0000

    version bump: - allowed users to download raw markdown from everywhere (to make more generic) - handling temporary directories nicer - updated the examples and documentation

--- curl.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-curl -G -d "per_page:1" \
-	-H "Accept: application/vnd.github+json" \
-	-H "Authorization: Bearer $(cat ./token.txt)" \
-	https://api.github.com/repos/mlabs-haskell/optim-onchain-audit/issues?per_page=100 &
-direction="asc"
--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 cabal run GHAppy -- \
 	-a $(cat ./token.txt) \
-	-o out -f outFile -r "mlabs-haskell/optim-onchain-audit" \
+	-o "out" \
+	-f "outFile" \
+	-r "mlabs-haskell/optim-onchain-audit" \
 	-u "cstml"
